e595fc50b95f88220fd785afca5519817c81bf45
Alex Odinayev <aodinaev@gmail.com>
2020-09-17 09:07:12 -0400

    collapsed some logic withing Device class

--- get_cdp_neighbors.py
+++ get_cdp_neighbors.py
@@ -1,28 +1,7 @@
 import getpass
 import pprint
 from napalm import get_network_driver
-
-def get_cdp_neighbors(napalm_driver, ip, username, password, **kwargs):
-    # in **kwargs could be an argument 'interface'
-    cli_str=[]
-    command='show cdp neighbors detail'
-    if 'interface' in kwargs:
-        command=f'show cdp neighbors {kwargs["interface"]} detail'
-    cli_str.append(command)
-
-    # NAPALM driver initialisation
-    driver = get_network_driver(napalm_driver)
-    ios = driver(ip, username, password)
-
-    ios.open()
-    ios.cli(['terminal length 0'])
-    result = ios.cli(cli_str)
-    ios.close()
-
-    result=result[command]
-    return result.split('\n')
-
-
+from collections import deque
 
 def parse_cdp(result):
     cdp=[]
@@ -34,7 +13,7 @@ def parse_cdp(result):
             n=1
             position = result[i+n].rfind(' ')
             neighbor["nbr_name"] = result[i+n][position+1:]
-            
+
 
             # parse neighbor ip
             n=n+2
@@ -58,84 +37,93 @@ def parse_cdp(result):
 
             position = result[i+n].rfind(' ')
             neighbor["nbr_int"]=result[i+n][position+1:]
-            
+
             cdp.append(neighbor)
-            
+
             # print(dev_name)
             # print(ip)
             # print(platform)
             # print(local_int)
             # print(remote_int)
-            
+
         i +=1
     return cdp
 
+@dataclass
+class Host:
+    name : str
+    ip : str
+    platform : str
+    cdp : dict = {}
+
+class Device:
+
+    def __init__(self, ip, username, password, *,driver_type='ios'):
+        klass = get_network_driver(driver_type)
+        self._driver = klass(ip, username, driver)
+
+    def __enter__(self):
+        self._driver.open()
+        return self
+
+    def __exit__(self, *exc_details):
+        self._driver.close()
+
+    @property
+    def facts(self):
+        return self._driver.get_facts()
+
+    def neighbors(self, *, interface=None):
+        if interface is None:
+            command = 'show cdp neighbors detail'
+        else:
+            command = f'show cdp neighbors {interface} detail'
+
+        self._driver.cli(['terminal length 0'])
+        result = self._driver.cli([command])
+        return result[command].split('\n')
+
 
 if __name__ == "__main__":
-    
+
     dev_ip = input('Enter device ip to start from: ')
     username = input("Enter Username: ")
     password = getpass.getpass()
 
-    driver = get_network_driver('ios')
-    ios = driver(dev_ip, username, password)
-    ios.open()
-    facts=ios.get_facts()
-    ios.close()
+    with Device(dev_ip, username, password) as device:
+        facts = device.facts
 
-    dev_name=facts["fqdn"]
-    dev_platform='cisco ' + facts['model']
-    # put hostname in result list of devices
+    queue = deque()
+    queue.append(Host(facts['fqdn'], dev_ip, 'cisco ' + facts['model']))
+    visited_hosts = set()
+    processed = []
 
-    
-    host = {}
-    host['dev_name'] = dev_name
-    host['dev_ip'] = dev_ip
-    host['dev_platform'] = dev_platform
-    host['dev_cdp'] = {}
+    while queue:
+        host = queue.pop()
+        print(f'connecting to {host.name}, ip: {hosts.ip}')
+        with Device(host.ip, username, password) as device:
+            result = device.neighbors()
+            print(f'Parsing cdp output for {host.name}')
+            host.cdp = parse_cdp(result)
+            # print('\n\n\n')
+            # pprint.pprint(hosts)
+            # print('\n')
+            # print(cdp)
 
-    hosts=[]
-    hosts.append(host)
-    # print(hosts)
-
-
-    i=0
-    known_hosts = {hosts[i]["dev_name"]}
-
-    while i < len(hosts):
-        print(f'connecting to {hosts[i]["dev_name"]}, ip: {hosts[i]["dev_ip"]}')
-        result=get_cdp_neighbors('ios', hosts[i]['dev_ip'], username, password)
-        print(f'Parsing cdp output for {hosts[i]["dev_name"]}')
-        cdp = parse_cdp(result)
-        hosts[i]['dev_cdp'] = cdp
-        # print('\n\n\n')
-        # pprint.pprint(hosts) 
-        # print('\n')
-        # print(cdp)
-
-
-        
-
+        processed.append(host)
+        visited_hosts.append(host.name)
 
         patterns = ['WS-C', 'C9200', 'C9300']
-        for item in cdp:
+        for item in host.cdp:
             # print(item)
             for pattern in patterns:
-                if pattern in item["nbr_platform"]:
-                    if item["nbr_name"] not in known_hosts:
-                        host = {}
-                        host['dev_name'] = item["nbr_name"]
-                        host['dev_ip'] = item["nbr_ip"]
-                        host['dev_platform'] = item["nbr_platform"]
-                        host['dev_cdp'] = {}
-                        hosts.append(host)
-                        known_hosts.add(item["nbr_name"])
-                        print(f'Host {item["nbr_name"]} has been added\n\n')
-        i +=1
+                if pattern in item["nbr_platform"] and item["nbr_name"] not in visited_hosts:
+                    queue.append(Host(item["nbr_name"], item["nbr_ip"], item["nbr_platform"]))
+                    print(f'Host {item["nbr_name"]} has been added\n\n')
 
-    pprint.pprint(hosts)
-    for host in hosts:
-        print(host['dev_name'])
+    pprint.pprint(processed)
+    for host in processed:
+        print(host.name)
 
 
 
