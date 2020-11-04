31d48f761371cb9b404c4abd871fa60ac44d409f
eudjinnl <eudjinn@gmail.com>
2020-11-04 16:22:52 +0300

    Added send command

--- nmk_send_command.py
+++ nmk_send_command.py
@@ -1,14 +1,40 @@
-from devactions import nmk_send_conf_command
+from devactions import Host, credentials_input, nmk_send_conf_command
+from get_cdp_neighbors import get_hosts_cdp_neighbors
+
+PATTERNS = ['C9200L']
 
 connection_params = {
                     'device_type': 'cisco_ios',
-                    'host': '10.1.1.3',
-                    'username': 'cisco',
-                    'password': 'cisco',
-                    'secret': 'cisco',
+                    'host': '',
+                    'username': '',
+                    'password': '',
+                    'secret': '',
                     'verbose': 'True'
                     }
 
-commands = ['username r4 secret r1']
-output = nmk_send_conf_command(connection_params, commands)
-print(output)
+with open("Cisco_commands.txt") as f:
+    commands = f.read().splitlines()
+
+start_ip = input('Enter device ip to start from: ')
+username, password, optional_args = credentials_input()
+
+connection_params['username'] = username
+connection_params['password'] = password
+connection_params['secret'] = optional_args['secret']
+
+hosts_cdp = get_hosts_cdp_neighbors(start_ip, username, password, optional_args)
+for host in hosts_cdp:
+    if PATTERNS:
+        for pattern in PATTERNS:
+            if pattern in host.platform:
+                connection_params['host'] = host.ip
+                output = nmk_send_conf_command(connection_params, commands)
+                print(output)
+    else:
+        connection_params['host'] = host.ip
+        output = nmk_send_conf_command(connection_params, commands)
+        print(output)
+
+# commands = ['username r4 secret r1']
+# output = nmk_send_conf_command(connection_params, commands)
+# print(output)
