5420f9d6af4d30741365643995add8a84b547325
eudjinnl <eudjinn@gmail.com>
2020-11-11 21:54:33 +0300

    Added time measuring

--- find_mac_address.py
+++ find_mac_address.py
@@ -1,10 +1,18 @@
 """
 Build list of all devices using cdp crawling.
 Find mac address in mac address table
-Return mac - device name, interface, vlan or some comments
-mac address can be enteres in any form
+Find mac address in mac table. Get cdp cdp neigbor of interface where mac address is.
+If there isn't switch there - mac address is found.
+If there is switch behind the interface connect to the switch.
+Repeat until mac address not found.
+If cdp shows that mac is on the neighbor with name we already checked 
+     that mac address is on non cisco switch which in between two cisco switches
+
+Returns mac - device name, interface, vlan or some comments
+mac address can be entered in any form
 """
 
+import time
 import netaddr
 from netaddr import mac_unix_expanded
 from devactions import Device, credentials_input
@@ -16,14 +24,17 @@ macs_input = input('Enter one or more MAC addresses (comma separated): ')
 start_ip = input('Enter device ip to start from: ')
 username, password, optional_args = credentials_input()
 
+start_time = time.clock()
+
 macs = macs_input.replace(' ', '')
 macs = macs.split(',')
 
 macs_to_find = []
 find_mac_result = []
 
-# Convertation form of mac address to form like AA:AA:AA:AA:AA:AA
-# Checking if mac address was entered or not based on it's form
+# Convertation form of mac address to form like AA:AA:AA:AA:AA:AA 
+# in which NAPALM is getting mac address table from device
+# Checking whether mac address was entered or not based on it's form
 for mac in macs:
     try:
         c_mac = netaddr.EUI(mac, dialect=mac_unix_expanded)
@@ -34,34 +45,43 @@ for mac in macs:
         find_mac_result.append(f'{mac} is not a Valid MAC address\n')
 
 for mac in macs_to_find:
-    dev = {}
+    dev = {} # results for device we connected to will be stored here
     cdp = []
     macdict = {}
     mac_found = False
     dev['dev_ip'] = start_ip
-    previous_dev = {"dev_name":"", "dev_ip":"", "interface":"", "vlan":""}
+    previous_dev = {"dev_name":"", "dev_ip":"", "interface":"", "vlan":""} # results for previous device
     while mac_found == False:
         try:
+            # Trying to connect to current device. On success getting device facts and mac table
             with Device(dev['dev_ip'], username, password, optional_args=optional_args) as device:
                 mac_table = device.mac_address_table
                 facts = device.facts
             dev['dev_name'] = facts["hostname"]
-        except:    
+        except:
+            # On failure    
             if previous_dev["dev_name"]:
-                # print(f'\n{mac} - {previous_dev["dev_name"]} {previous_dev["dev_ip"]} interface {previous_dev["interface"]} Vlan{previous_dev["vlan"]}')
+                # If results from previous device exist they are results of finding this mac address
                 find_mac_result.append(f'\n{mac} - {previous_dev["dev_name"]} {previous_dev["dev_ip"]} interface {previous_dev["interface"]} Vlan{previous_dev["vlan"]}')
                 break
             else:
+                # If results from previous device don't exist we can't connect to the first switch.
                 print('Unable to connect to the first device')
                 break
         
         known_macs = {}
+        # Getting set of mac addresses from current switch mac table
         known_macs = {macdict["mac"] for macdict in mac_table}
         cdp = {}
 
         if mac in  known_macs:
+            # If mac address is in mac table of current switch
+            # find dictionary from mac_table with mac address we are looking for 
             for macdict in mac_table:
                 if macdict["mac"] == mac:
+                    # When the dictionary was found check interface in it.
+                    # If dictionary refers to interface PortChannel 
+                    #    find out physical interface in the PortChannel interface
                     if 'Po' in macdict["interface"]:
                         with Device(dev["dev_ip"], username, password, optional_args=optional_args) as device:
                             command = [f'show interface {macdict["interface"]} etherchannel']
@@ -71,6 +91,7 @@ for mac in macs_to_find:
                                 try:
                                     interface = parse_int_name(res_str)
                                     if 'Port-channel' not in interface:
+                                        # rewrite PortChannel interface with found physical interface in macdict
                                         macdict["interface"] = interface
                                         break
                                 except:
@@ -111,10 +132,15 @@ for mac in macs_to_find:
                         find_mac_result.append(f'\n{mac} - {dev["dev_name"]} {dev["dev_ip"]} interface {dev["interface"]} Vlan{dev["vlan"]}')
                         break
         else:
+            # If there isn't mac address in mac table of current switch - end of search of this mac address.
             mac_found = True
             # print(f'\n{mac} has not been found')
             find_mac_result.append(f'\n{mac} has not been found')   
 
+time_end = time.clock()
+
 print('RESULTS')
 for item in find_mac_result:
     print(item) 
+
+print(time_end - start_time, "seconds")
